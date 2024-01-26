73b7ce302f1fe7e985bc967559c4359962785148
safdarali01 <safdarali.2699@gmail.com>
2024-01-26 13:23:26 +0500

    Service Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Category from "./components/Category/Category"
 import Category2 from "./components/Category/Category2"
 import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
+import Services from "./components/Services/Services"
 
 function App() {
 
   return (
-    <>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <Navbar/>
       <Hero/>
       <Category/>
       <Category2/>
-    </>
+      <Services/>
+    </div>
   )
 }
 
